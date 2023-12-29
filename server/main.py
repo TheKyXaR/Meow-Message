d9d1d9612193a7f9eec4a7ec8a303b839882e394
The_Povar <73489877+VipOleg@users.noreply.github.com>
2023-12-30 01:04:00 +0200

    V 0.5 B
    Server and user update. Add function select and insert.
    --------------------
    Fix ouput data (in user and server).

---
+++ server/main.py
@@ -0,0 +1,44 @@
+# Server
+
+import keyboard
+import socket
+import json
+import db
+
+sock = socket.socket()
+sock.bind(('127.0.0.1', 9090))
+sock.listen()
+
+def close_server () :
+	sock.close()
+keyboard.add_hotkey("ctrl+shift+c", close_server)
+
+def send_message (user, data) :
+	user.sendall(data)
+
+while True :
+	user, addr = sock.accept()
+
+	data = user.recv(1024).decode("utf-8")
+	data = json.loads(data)
+
+	if data['command'] == "select" :
+		res = db.select()
+
+	elif data['command'] == "insert" :
+		res = db.insert(data)
+		# print(data)
+
+	print(res)
+
+	try:
+		user.send(json.dumps(res).encode("utf-8"))
+	except :
+		user.send(str(res).encode("utf-8"))
+	
+
+	# try:
+	# 	user.send(json.dumps(data).encode("utf-8"))
+	# except :
+	# 	user.send("False".encode("utf-8"))
+	
---
+++ user/main.py
@@ -0,0 +1,92 @@
+# User
+
+import socket
+import json
+import time
+import keyboard
+from os import system as sys
+
+def server (message) :
+	if not message :
+		return
+
+	sock = socket.socket()
+	sock.connect(('127.0.0.1', 9090))
+
+	sock.send(message.encode("utf-8"))
+
+	data = sock.recv(1024)
+	return data.decode("utf-8")
+	sock.close()
+
+data = {
+	"command": "select",
+	"data": ""
+}
+
+def input_message () :
+	global data
+
+	message = input()
+	data["command"] = "insert"
+	data["data"] = message
+	res = json.dumps(data)
+	server(res)
+
+def main () :
+	global data
+
+	keyboard.add_hotkey("enter", input_message)
+
+	print("Hello, welcome to Meow Message")
+	# data['nickname'] = input("enter your nick name - ")
+	data['nickname'] = "admin"
+
+	check_message_time = time.time()
+
+	while True :
+		if time.time() - check_message_time > 0.5 :
+			check_message_time = time.time()
+
+			# sys("cls")
+			data["command"] = "select"
+			res = json.dumps(data)
+			print(server(res))
+
+		
+
+if __name__ == '__main__':
+	main()
+
+
+"""
+
+{
+	"command": "select",
+	"sql": {
+		"select": "*",
+		"from" : "table",
+		"where": "",
+	}
+	"data": {
+		"login": input(),
+		"password": input(),
+	}
+}
+
+"""
+
+
+# while True :
+# 	sock = socket.socket()
+# 	sock.connect(('127.0.0.1', 9090))
+
+# 	message = input()
+# 	if not message :
+# 		message = "empty box"
+
+# 	if message == "end connection" :
+# 		break
+# 	sock.send(message.encode("utf-8"))
+
+# sock.close()
